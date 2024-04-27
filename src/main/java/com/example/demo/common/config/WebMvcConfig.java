25e2b26a5ce314e80fe90aa11e8feaa36d67f50a
awakelife93 <awakelife93@gmail.com>
2024-04-27 19:57:54 +0900

    refactor: update WebMvcConfig

--- src/main/java/com/example/demo/common/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/common/config/WebMvcConfig.java
@@ -2,19 +2,11 @@ package com.example.demo.common.config;
 
 import com.example.demo.common.interceptor.LoggingForInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/**");
-  }
+public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
