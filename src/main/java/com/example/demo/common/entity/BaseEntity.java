523597a81377b95cb748f0e891f81f22bb34d70f
awakelife93 <awakelife93@gmail.com>
2024-04-30 15:53:30 +0900

    refactor: add Serializable to BaseEntity for batch parameter serving

--- src/main/java/com/example/demo/common/entity/BaseEntity.java
+++ src/main/java/com/example/demo/common/entity/BaseEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
@@ -16,7 +17,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
+public abstract class BaseEntity implements Serializable {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
