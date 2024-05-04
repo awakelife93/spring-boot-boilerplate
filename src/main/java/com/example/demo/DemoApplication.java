ae7d921e6d0ad58b94b37f5e078f9484b1bf7475
awakelife93 <awakelife93@gmail.com>
2024-05-04 18:44:26 +0900

    refactor: separate out EnableJpaAuditing Configuration

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
 public class DemoApplication {
--- src/main/java/com/example/demo/common/config/JpaAuditConfig.java
+++ src/main/java/com/example/demo/common/config/JpaAuditConfig.java
@@ -4,7 +4,9 @@ import com.example.demo.security.service.impl.AuditorAwareImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @Configuration
 public class JpaAuditConfig {
 
